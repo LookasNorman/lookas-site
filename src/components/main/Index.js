3a2aefc4c10e329a40cbddbf138acc1f845e1fe4
LookasNorman <lookasziebice@gmail.com>
2020-12-14 22:32:41 +0100

    Added padding

--- src/components/main/Index.js
+++ src/components/main/Index.js
@@ -9,11 +9,7 @@ import Courses from './Courses'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
   },
 }))
 
