a4a6b9eac4865e27c3ddcb7e261037b30807d3ae
LookasNorman <lookasziebice@gmail.com>
2020-12-14 16:34:12 +0100

    Change list item to acording

--- src/components/main/Courses.js
+++ src/components/main/Courses.js
@@ -6,8 +6,17 @@ import Typography from '@material-ui/core/Typography'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import SchoolIcon from '@material-ui/icons/School'
-import { Divider, ListItem, ListItemText } from '@material-ui/core'
+import {
+  Accordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionSummary,
+  Divider,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core'
 import Chip from '@material-ui/core/Chip'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,10 +40,24 @@ const useStyles = makeStyles((theme) => ({
   inline: {
     display: 'inline',
   },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '90%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
 }))
 
 export default function Courses() {
   const classes = useStyles()
+  const [expanded, setExpanded] = React.useState(false)
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false)
+  }
 
   return (
     <Card className={classes.root}>
@@ -49,53 +72,57 @@ export default function Courses() {
         </ListItem>
         <CardContent className={classes.content}>
           <ListItem>
-            <ListItemText
-              primary="Coders Lab JavaScript: React + Redux"
-              secondary={
-                <React.Fragment>
-                  <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
-                    nr dyplomu 4639/2019
-                  </Typography>
-                  <div className={classes.chip}>
-                    <Chip label="React" />
-                    <Chip label="Redux" />
-                  </div>
-                </React.Fragment>
-              }
-            />
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
+                <Typography className={classes.heading}>Coders Lab JavaScript: React + Redux</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
+                  nr dyplomu 4639/2019
+                </Typography>
+              </AccordionDetails>
+              <AccordionActions>
+                <div className={classes.chip}>
+                  <Chip label="React" />
+                  <Chip label="Redux" />
+                </div>
+              </AccordionActions>
+            </Accordion>
           </ListItem>
           <Divider variant="inset" />
           <ListItem>
-            <ListItemText
-              primary="Coders Lab Backend Developer PHP"
-              secondary={
-                <React.Fragment>
-                  <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
-                    nr dyplomu 4120/2019
-                  </Typography>
-                  <div className={classes.chip}>
-                    <Chip label="PHP" />
-                    <Chip label="MySQL" />
-                    <Chip label="Symfony" />
-                    <Chip label="JavaScript" />
-                    <Chip label="Scrum" />
-                  </div>
-                </React.Fragment>
-              }
-            />
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel2bh-content" id="panel2bh-header">
+                <Typography className={classes.heading}>Coders Lab Backend Developer PHP</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
+                  nr dyplomu 4120/2019
+                </Typography>
+              </AccordionDetails>
+              <AccordionActions>
+                <div className={classes.chip}>
+                  <Chip label="PHP" />
+                  <Chip label="MySQL" />
+                  <Chip label="Symfony" />
+                  <Chip label="JavaScript" />
+                  <Chip label="Scrum" />
+                </div>
+              </AccordionActions>
+            </Accordion>
           </ListItem>
           <Divider variant="inset" />
           <ListItem>
-            <ListItemText
-              primary="Akademia ekonomiczna"
-              secondary={
-                <React.Fragment>
-                  <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
-                    Informatyka i ekonometria. Zarządzanie informacją i wiedzą. Studia przerwane na 5 roku.
-                  </Typography>
-                </React.Fragment>
-              }
-            />
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel3bh-content" id="panel3bh-header">
+                <Typography className={classes.heading}>Akademia ekonomiczna - Kraków / Wrocław</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
+                  Informatyka i ekonometria. Zarządzanie informacją i wiedzą. Studia przerwane na 5 roku.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
           </ListItem>
         </CardContent>
       </div>
