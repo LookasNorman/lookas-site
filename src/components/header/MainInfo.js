524b70a727a8dcc0258867540095f0d71e5426af
LookasNorman <lookasziebice@gmail.com>
2020-12-18 21:48:46 +0100

    Delete bzdrive file

--- src/components/header/MainInfo.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import CardHeader from '@material-ui/core/CardHeader'
-
-export const MainInfo = ({data}) => {
-    return (
-        <CardHeader title={data.title} subheader={data.subheader} />
-    )
-}
